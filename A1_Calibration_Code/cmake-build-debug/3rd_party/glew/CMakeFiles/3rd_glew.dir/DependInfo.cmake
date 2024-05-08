ccb95c23aead2cbd57542a425c03e7e2c26a736f
Zhuoyuee <zhuoyueww@gmail.com>
2024-05-08 15:28:07 +0200

    A

---
+++ A1_Calibration_Code/cmake-build-debug/3rd_party/glew/CMakeFiles/3rd_glew.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/GEO1016/Geo1016-photogrammetry/A1_Calibration_Code/3rd_party/glew/src/glew.c" "3rd_party/glew/CMakeFiles/3rd_glew.dir/src/glew.c.obj" "gcc" "3rd_party/glew/CMakeFiles/3rd_glew.dir/src/glew.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
