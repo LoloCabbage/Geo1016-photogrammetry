286bc5fff394c08bdb3eab64d2b36ac27e4bf5dc
LoloCabbage <108572718+LoloCabbage@users.noreply.github.com>
2024-05-19 18:20:42 +0200

    step1

--- A2_Triangulation_Code/Triangulation/triangulation_method.cpp
+++ A2_Triangulation_Code/Triangulation/triangulation_method.cpp
@@ -35,6 +35,121 @@ using namespace easy3d;
  * @return True on success, otherwise false. On success, the reconstructed 3D points must be written to 'points_3d'
  *      and the recovered relative pose must be written to R and t.
  */
+
+//// struct for normalization information
+struct normalization {
+    Vector2D centroid; // image center
+    double avg_dist; // average distance to the image center
+    std::vector<Vector2D> normal_points; // normalized image points
+    Matrix T; // transformation matrix
+};
+
+//// check if the input is valid
+bool check_input(const std::vector<Vector2D>& points_0, const std::vector<Vector2D>& points_1) {
+    // check if the number of correspondences >= 8
+    if (points_0.size() < 8 || points_1.size() < 8) {
+        std::cerr << "Error: the number of correspondences must be at least 6." << std::endl;
+        return false;
+    }
+
+        // check if the sizes of 2D/3D points match
+    else if (points_0.size() != points_1.size()) {
+        std::cerr << "Error: the sizes of 2D/3D points must match." << std::endl;
+        return false;
+    }
+
+    std::cout << "Input data is valid." << std::endl;
+    return true;
+}
+
+//// normalize the points
+normalization normalize(const std::vector<Vector2D>& points){
+    std::vector<Vector2D> normal_points;
+    // compute the centroid of the points
+    Vector2D centroid(0, 0);
+    for (const auto& point : points) {
+        centroid += point;
+    }
+    centroid /= points.size();
+    // use the centroid as the origin
+    for (const auto& point : points) {
+        normal_points.push_back(point - centroid);
+    }
+    // compute the average distance to the origin
+    double avg_dist = 0;
+    for (const auto& point : normal_points) {
+        avg_dist += point.norm();
+    }
+    avg_dist /= normal_points.size();
+    // scale the points so that the average distance to the origin is sqrt(2)
+    for (auto& point : normal_points) {
+        point *= sqrt(2) / avg_dist;
+    }
+
+    // compute the transformation matrix
+    Matrix T(3, 3);
+    T.set(0, 0, sqrt(2) / avg_dist);
+    T.set(1, 1, sqrt(2) / avg_dist);
+    T.set(2, 2, 1);
+    T.set(0, 2, -centroid.x() * sqrt(2) / avg_dist);
+    T.set(1, 2, -centroid.y() * sqrt(2) / avg_dist);
+
+    std::cout << "T: " << T << std::endl;
+
+    normalization norm_data;
+    norm_data.centroid = centroid;
+    norm_data.avg_dist = avg_dist;
+    norm_data.normal_points = normal_points;
+    norm_data.T = T;
+    return norm_data;
+}
+
+//// generate the fundamental matrix
+Matrix F (const std::vector<Vector2D>& normal_points_0, const std::vector<Vector2D>& normal_points_1){
+    int n = normal_points_0.size();
+    Matrix A(n, 9);
+    for (int i = 0; i < n; i++) {
+        double x0 = normal_points_0[i].x();
+        double y0 = normal_points_0[i].y();
+        double x1 = normal_points_1[i].x();
+        double y1 = normal_points_1[i].y();
+
+        A(i, 0) = x0 * x1;
+        A(i, 1) = y0 * x1;
+        A(i, 2) = x1;
+        A(i, 3) = x0 * y1;
+        A(i, 4) = y0 * y1;
+        A(i, 5) = y1;
+        A(i, 6) = x0;
+        A(i, 7) = y0;
+        A(i, 8) = 1;
+    }
+
+    // svd decomposition
+    Matrix U(n, n);
+    Matrix D(n, 9);
+    Matrix V(9, 9);
+    svd_decompose(A, U, D, V);
+    int numCols = V.cols();
+    Vector F = V.get_column(numCols - 1);
+    Matrix F_matrix(3, 3, F.data());
+
+    // enforce rank 2
+    Matrix U_F(3, 3);
+    Matrix D_F(3, 3);
+    Matrix V_F(3, 3);
+    svd_decompose(F_matrix, U_F, D_F, V_F);
+    D_F.set(2, 2, 0);
+    F_matrix = U_F * D_F * V_F.transpose(); //// TRANSPOSE OR NOT?? to CHECK
+    std::cout << "F_matrix: " << F_matrix << std::endl;
+    return F_matrix;
+}
+
+//// denormalize the fundamental matrix
+Matrix denormalize(const Matrix& F, const Matrix& T0, const Matrix& T1){
+    return T1.transpose() * F * T0;
+}
+
 bool Triangulation::triangulation(
         double fx, double fy,     /// input: the focal lengths (same for both cameras)
         double cx, double cy,     /// input: the principal point (same for both cameras)
@@ -46,112 +161,29 @@ bool Triangulation::triangulation(
         Vector3D &t    /// output: 3D vector, which is the recovered translation of the 2nd camera
 ) const
 {
-    /// NOTE: there might be multiple workflows for reconstructing 3D geometry from corresponding image points.
-    ///       This assignment uses the commonly used one explained in our lecture.
-    ///       It is advised to define a function for the sub-tasks. This way you have a clean and well-structured
-    ///       implementation, which also makes testing and debugging easier. You can put your other functions above
-    ///       triangulation(), or put them in one or multiple separate files.
-
-    std::cout << "\nTODO: I am going to implement the triangulation() function in the following file:" << std::endl
-              << "\t    - triangulation_method.cpp\n\n";
-
-    std::cout << "[Liangliang]:\n"
-                 "\tFeel free to use any provided data structures and functions. For your convenience, the\n"
-                 "\tfollowing three files implement basic linear algebra data structures and operations:\n"
-                 "\t    - Triangulation/matrix.h  Matrices of arbitrary dimensions and related functions.\n"
-                 "\t    - Triangulation/vector.h  Vectors of arbitrary dimensions and related functions.\n"
-                 "\t    - Triangulation/matrix_algo.h  Determinant, inverse, SVD, linear least-squares...\n"
-                 "\tPlease refer to the above files for a complete list of useful functions and their usage.\n\n"
-                 "\tIf you choose to implement the non-linear method for triangulation (optional task). Please\n"
-                 "\trefer to 'Tutorial_NonlinearLeastSquares/main.cpp' for an example and some explanations.\n\n"
-                 "\tIn your final submission, please\n"
-                 "\t    - delete ALL unrelated test or debug code and avoid unnecessary output.\n"
-                 "\t    - include all the source code (and please do NOT modify the structure of the directories).\n"
-                 "\t    - do NOT include the 'build' directory (which contains the intermediate files in a build step).\n"
-                 "\t    - make sure your code compiles and can reproduce your results without ANY modification.\n\n" << std::flush;
-
-    /// Below are a few examples showing some useful data structures and APIs.
-
-    /// define a 2D vector/point
-    Vector2D b(1.1, 2.2);
-
-    /// define a 3D vector/point
-    Vector3D a(1.1, 2.2, 3.3);
-
-    /// get the Cartesian coordinates of a (a is treated as Homogeneous coordinates)
-    Vector2D p = a.cartesian();
-
-    /// get the Homogeneous coordinates of p
-    Vector3D q = p.homogeneous();
-
-    /// define a 3 by 3 matrix (and all elements initialized to 0.0)
-    Matrix33 A;
-
-    /// define and initialize a 3 by 3 matrix
-    Matrix33 T(1.1, 2.2, 3.3,
-               0, 2.2, 3.3,
-               0, 0, 1);
-
-    /// define and initialize a 3 by 4 matrix
-    Matrix34 M(1.1, 2.2, 3.3, 0,
-               0, 2.2, 3.3, 1,
-               0, 0, 1, 1);
-
-    /// set first row by a vector
-    M.set_row(0, Vector4D(1.1, 2.2, 3.3, 4.4));
-
-    /// set second column by a vector
-    M.set_column(1, Vector3D(5.5, 5.5, 5.5));
-
-    /// define a 15 by 9 matrix (and all elements initialized to 0.0)
-    Matrix W(15, 9, 0.0);
-    /// set the first row by a 9-dimensional vector
-    W.set_row(0, {0, 1, 2, 3, 4, 5, 6, 7, 8}); // {....} is equivalent to a std::vector<double>
-
-    /// get the number of rows.
-    int num_rows = W.rows();
-
-    /// get the number of columns.
-    int num_cols = W.cols();
-
-    /// get the the element at row 1 and column 2
-    double value = W(1, 2);
-
-    /// get the last column of a matrix
-    Vector last_column = W.get_column(W.cols() - 1);
-
-    /// define a 3 by 3 identity matrix
-    Matrix33 I = Matrix::identity(3, 3, 1.0);
-
-    /// matrix-vector product
-    Vector3D v = M * Vector4D(1, 2, 3, 4); // M is 3 by 4
-
-    ///For more functions of Matrix and Vector, please refer to 'matrix.h' and 'vector.h'
-
-    // TODO: delete all above example code in your final submission
-
-    //--------------------------------------------------------------------------------------------------------------
-    // implementation starts ...
-
     // TODO: check if the input is valid (always good because you never known how others will call your function).
+    bool valid = check_input(points_0, points_1);
+    if (!valid){
+        return false;
+    }
 
     // TODO: Estimate relative pose of two views. This can be subdivided into
     //      - estimate the fundamental matrix F;
-    //      - compute the essential matrix E;
-    //      - recover rotation R and t.
+    normalization norm_data_0 = normalize(points_0);
+    normalization norm_data_1 = normalize(points_1);
+    std::vector<Vector2D> normal_points_0 = norm_data_0.normal_points;
+    std::vector<Vector2D> normal_points_1 = norm_data_1.normal_points;
+
+    Matrix F_matrix = F(normal_points_0, normal_points_1);
+    Matrix F_denormalized = denormalize(F_matrix, norm_data_0.T, norm_data_1.T);
+
+    // TODO: - compute the essential matrix E;
+
+    // TODO: - recover rotation R and t.
 
     // TODO: Reconstruct 3D points. The main task is
     //      - triangulate a pair of image points (i.e., compute the 3D coordinates for each corresponding point pair)
 
-    // TODO: Don't forget to
-    //          - write your recovered 3D points into 'points_3d' (so the viewer can visualize the 3D points for you);
-    //          - write the recovered relative pose into R and t (the view will be updated as seen from the 2nd camera,
-    //            which can help you check if R and t are correct).
-    //       You must return either 'true' or 'false' to indicate whether the triangulation was successful (so the
-    //       viewer will be notified to visualize the 3D points and update the view).
-    //       There are a few cases you should return 'false' instead, for example:
-    //          - function not implemented yet;
-    //          - input not valid (e.g., not enough points, point numbers don't match);
-    //          - encountered failure in any step.
+
     return points_3d.size() > 0;
 }
